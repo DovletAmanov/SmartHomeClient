c8cd814b5d6eeca6cdc7f99294747f49330fe3f5
Dovlet Amanov <turkmenium@gmail.com>
2018-02-19 23:21:32 +0300

    Heat/Humidity data retrieve: success

--- src/components/HomeStatusBody/HomeStatusScreen.js
+++ src/components/HomeStatusBody/HomeStatusScreen.js
@@ -1,14 +1,44 @@
 import React, {Component} from 'react';
+import socketIOClient from "socket.io-client";
 import Auxiliary from '../hoc/Auxiliary';
 
 class HomeStatusScreen extends Component{
 
+    constructor() {
+        super();
+        this.state = {
+          temperature: false,
+          humidity: false,
+          endpoint: "http://192.168.1.103:8888"
+        };
+      }
+
+      componentDidMount() {
+        const { endpoint } = this.state;
+        const socket = socketIOClient(endpoint);
+        socket.on("sendingheatdata", data => this.setState({ temperature: data.temperature, humidity: data.humidity }));
+      }
+
     render(){
-        return(
-            <Auxiliary>
-            <div>Hello Im HomeStatusScreen :)</div>
-            </Auxiliary>
-        )
+        const { temperature, humidity } = this.state;
+       
+        return (
+          <div style={{ textAlign: "center" }}>
+
+            {temperature
+              ? <p>
+                   Temperature: {temperature} Celsius 
+                </p>
+              : <p>Loading...</p>}
+
+              {humidity
+              ? <p>
+                   Humidity: {humidity} %
+                </p>
+              : <p>Loading...</p>}
+
+          </div>
+        );
     }
 }
 
